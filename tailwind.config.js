271068a3305c381e249b0a3cead29081aad104bb
DanielRodado <d4nielrodado@gmail.com>
2024-07-05 19:04:15 -0500

    Personalización de Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,14 @@ module.exports = {
             red: "hsl(1, 90%, 64%)",
             blue: "hsl(219, 85%, 26%)",
             white: "hsl(0, 0%, 100%)",
-            "very-light-grayish-blue": "hsl(210, 60%, 98%)",
-            "light-grayish-blue": {
-                "Light-grayish-blue-1": "hsl(211, 68%, 94%)",
-                "Light-grayish-blue-2": "hsl(205, 33%, 90%)",
+            "grayish-blue-very-light": "hsl(210, 60%, 98%)",
+            "grayish-blue-light": {
+                1: "hsl(211, 68%, 94%)",
+                2: "hsl(205, 33%, 90%)",
             },
             "grayish-blue": "hsl(219, 14%, 63%)",
-            "dark-grayish-blue": "hsl(219, 12%, 42%)",
-            "very-dark-blue": "hsl(224, 21%, 14%)",
+            "grayish-blue-dark": "hsl(219, 12%, 42%)",
+            "blue-very-dark": "hsl(224, 21%, 14%)",
         },
         extend: {
             fontFamily: {
