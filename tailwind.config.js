ba2404916fa60196eb4814f326e573de63e60010
DanielRodado <d4nielrodado@gmail.com>
2024-07-08 17:43:26 -0500

    Se movió 'index.html' a la carpeta public

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./index.html", "./src/**/*.{js,css}"],
+    content: ["./public/**/*.html", "./src/**/*.{js,css}"],
     theme: {
         colors: {
             red: "hsl(1, 90%, 64%)",
